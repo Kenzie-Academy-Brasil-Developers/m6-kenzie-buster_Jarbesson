73afc2f0d8691ebc5c8d4efb2ceb54458bb2fbff
Jarbesson <jarbessondasilvarodrigues@gmail.com>
2024-04-29 18:18:03 -0400

    tarefa 3 e 4 concluida

--- movies/permissions.py
+++ movies/permissions.py
@@ -1,9 +1,14 @@
 from rest_framework import permissions
 from rest_framework.views import Request, View
-from movies.models import Movie
+# from movies.models import Movie
 
 
 class IsBookOwner(permissions.BasePermission):
-    def has_object_permission(self, request: Request, view: View, obj: Movie):
-        return obj.added_by == request.user
+    # def has_object_permission(self, request: Request, view: View, obj: Movie):
+    #     return obj.added_by == request.user
     
+    def has_permission(self, req: Request, view: View):
+        if req.method == "GET":
+            return True
+        if req.user.is_authenticated and req.user.is_superuser:
+            return True
